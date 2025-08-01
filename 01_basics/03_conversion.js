1b7ce95d67f9acfe6642b4698dd92b7161f058c4
Chaitanya Ajabe <chaitanyaajabe7@gmail.com>
2025-08-01 19:38:50 +0000

    Conversion & Comaprison

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,35 @@
+
+let age = 23;
+let age1 = "2a";
+
+let convertedNo = Number(age1);
+
+console.table([typeof(age),typeof(age1),typeof convertedNo,convertedNo]);
+
+// ------------------------- Boolean ------//
+isloggedIn = 1;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+
+console.table([typeof isloggedIn, typeof booleanIsLoggedIn]);
+
+
+// ------------------------- String ------//
+let num1 = 33;
+let stringNum = String(num1);
+
+console.table([typeof num1, num1 , typeof stringNum, stringNum]);
+
+//// ------ Operations ----- ////
+let value = 3
+let negativeValue = -value;
+
+console.table([typeof value, value, typeof negativeValue, negativeValue]);
+
+//// ------------- /////
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+//console.log("2" > 1)  /// "2" converted to Number Internally
+//console.log("01" > 1) /// "1" converted to Number Internally
+console.log("2" === 2)
+
+
+/// ------ null  --------------/// Some time convert into Null/0
+console.log(null > 0)
+console.log(null == 0)
+console.log(null === 0)  // value check + dataType Check
+console.log(null < 0)
