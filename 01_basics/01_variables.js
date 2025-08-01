ae1201b4619a2cbc093deca418fa2d78fe0c3e79
Chaitanya Ajabe <chaitanyaajabe7@gmail.com>
2025-08-01 18:55:41 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 123456789;
+
+/*
+Prefer Not use Var
+Its Support Functional Scope than Block Scope
+*/
+let name = "Chaitanya";
+var salary = 20000;
+
+/// Dynamic 
+techStack = "Java";
+
+/// Undefined
+let BackUpSource;
+
+
+//// priinting
+// 1
+console.log(accountId);
+
+// 2
+console.table([accountId,name,salary,techStack,BackUpSource]);
+
