5ecde29a35e8e90fa46ee19a9d77fdfe29a59c55
Chaitanya Ajabe <chaitanyaajabe7@gmail.com>
2025-08-01 18:55:18 +0000

    Installation Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat All Js Code as a newer Version
+
+//alert(2 +5)  // Error as we are using Node js not Browser
+
+/// Data Types
+
+/// Premitive DataTypes
+/*
+1) Number
+2) bigint
+3) string
+4) boolean
+5) null
+6) undefined
+7) symbol
+*/
+
+/// Objects/NonPremitive
+/*
+1) array
+2) object
+3) function
+*/
+
+
