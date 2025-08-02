9d486630b867e33ce5c5ce17d08ceda58f5f49cb
Chaitanya Ajabe <chaitanyaajabe7@gmail.com>
2025-08-02 10:16:46 +0000

    nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,15 @@
+/// 2 ways
+const score = 100
+let varScore = new Number(80)
+
+//// Num to String
+console.log(typeof(varScore.toString()))
+console.log(varScore.toFixed(3))
+
+let deciNo = 12.563
+console.log(deciNo.toPrecision(4))
+
+/// -------- Nums & Math -------- ///
+
+console.log(Math.sqrt(4));
+console.log(Math.max(5, 6, 2 ,9))
