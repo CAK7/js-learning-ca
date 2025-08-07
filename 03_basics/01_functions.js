d1632a1e5fc5a04e9de8db7973c7d3d988a83a6c
Chaitanya Ajabe <chaitanyaajabe7@gmail.com>
2025-08-07 20:27:56 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,41 @@
+///// Example 1
+// function myFunction(){
+//     console.log("My Function");
+    
+// }
+// myFunction()
+
+// ///// Add Two Number
+// function addTwoNum(num1,num2){
+//     return num1+num2
+// }
+// console.log(addTwoNum(5,6))
+
+////// String Function with type check
+// function stringCheck(variable){
+//     if(!variable){
+//         console.log("Send Valid String");
+//         return ""
+//     }
+//     return `My Name is ${variable}`
+// }
+// console.log(stringCheck("Chaitanya"))
+
+
+///// unknown no of parameter
+// function cartValue(...nums){
+//     return nums
+// }
+
+// console.log(cartValue(50,40,30));
+
+
+//// Object Para
+// function objectInput(emp){
+//     console.log(`Emp Name is ${emp.name} and Id is ${emp.Id}`)
+//     return emp
+// }
+// objectInput({
+//     name :"Chaitanya",
+//     Id : 1
+// })
